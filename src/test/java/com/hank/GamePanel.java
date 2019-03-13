c4c9a8822ff8152ac395dab279adfad1c9f806ff
friendlyhank <hanguang8888@outlook.com>
2019-03-13 13:53:25 +0800

    GamePanel

--- src/test/java/com/hank/GamePanel.java
+++ src/test/java/com/hank/GamePanel.java
@@ -1,14 +1,35 @@
 package com.hank;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
 
-public class JFrameTest {
+//(1)extends JPanel面板
+public class GamePanel extends JPanel{
+
+
+        @Override
+        public void paint(Graphics g){
+            super.paint(g);
+            System.out.println("绘制面板操作");
+        }
+
+
+    //(2)extends KeyAdapter
+    public class MyKeyLinenter extends KeyAdapter{
+
+    }
+
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
         jFrame.setSize(800,630);
         jFrame.setLocationRelativeTo(null);//窗口位置
         jFrame.setResizable(true);//窗口可由用户自由调整
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//用户单击关闭按钮执行的动作
+
+        GamePanel gamePanel = new GamePanel();
+        jFrame.add(gamePanel);
+
         jFrame.setVisible(true);//设置可见
     }
 }
